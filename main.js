6cccc3b3fbce999d83cbafc819690ed4c7a1a81c
Devarsh-arch <81173191+Devarsh-arch@users.noreply.github.com>
2022-01-11 16:06:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+function preload(){
+
+}
+function setup(){
+    canvas= createCanvas(600, 400);
+    canvas.center();
+    video= createCapture(VIDEO);
+    video.size(600, 400);
+    video.hide();
+    video.center()
+
+    poseNet= ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function gotPoses(results){
+    console.log(results);
+
+    noseX= results[0].pose.nose.x;
+    noseY= results[0].pose.nose.y;
+}
+function modelLoaded(){
+    console.log("poseNet Is Working");
+}
+function draw(){
+    image(video, 0, 0, 600, 400);
+}
+function takesnapshot(){
+    save('Project.png');
+}
